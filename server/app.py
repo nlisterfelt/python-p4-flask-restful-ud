0b6f96edfcdf76622cfeec78213ed83b9959d6d3
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-11-26 15:34:54 -0600

    add patch and delete

--- server/app.py
+++ server/app.py
@@ -19,69 +19,50 @@ api = Api(app)
 class Home(Resource):
 
     def get(self):
-        
         response_dict = {
             "message": "Welcome to the Newsletter RESTful API",
         }
-        
-        response = make_response(
-            response_dict,
-            200,
-        )
-
-        return response
+        return make_response(response_dict.to_dict(), 200)
 
 api.add_resource(Home, '/')
 
 class Newsletters(Resource):
 
     def get(self):
-        
         response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
-
-        response = make_response(
-            response_dict_list,
-            200,
-        )
-
-        return response
+        return make_response(response_dict_list, 200)
 
     def post(self):
-        
         new_record = Newsletter(
             title=request.form['title'],
             body=request.form['body'],
         )
-
         db.session.add(new_record)
         db.session.commit()
-
-        response_dict = new_record.to_dict()
-
-        response = make_response(
-            response_dict,
-            201,
-        )
-
-        return response
+        return make_response(new_record.to_dict(), 201)
 
 api.add_resource(Newsletters, '/newsletters')
 
 class NewsletterByID(Resource):
-
     def get(self, id):
-
         response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+        return make_response(response_dict, 200)
+    
+    def patch(self, id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(newsletter, attr, request.form[attr])
+        db.commit()
+        return make_response(newsletter.to_dict(), 200)
 
-        response = make_response(
-            response_dict,
-            200,
-        )
-
-        return response
+    def delete(self, id):
+        newsletter = Newsletter.query.filter_by(id=id).first()
+        db.session.delete(newsletter)
+        db.session.commit()
+        response = {"message": "Newsletter successfully deleted."}
+        return make_response(response, 200)
 
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
